34d5bdb74521378072608507c3428921545c86cc
ConnorStude <116616279+ConnorStude@users.noreply.github.com>
2024-02-20 22:58:15 -0800

    Add files via upload

---
+++ traffic_lights.h
@@ -0,0 +1,11 @@
+#include "TM4C123GH6PM.h"
+
+void traffic_init(void);
+void PLL_init(void);
+void Red_Green(void);
+void Green_Red(void);
+void Red_Red(void);
+void Red_Yellow(void);
+void Yellow_Red(void);
+void SysTick_Init(void);
+void SysTick_Wait (uint32_t delay);
