8bc3c83066c15e6fc09ef5aac44b621ce61dc82a
ConnorStude <116616279+ConnorStude@users.noreply.github.com>
2024-02-21 17:28:38 -0800

    Add files via upload
    these work

--- traffic_lights.c
+++ traffic_lights.c
@@ -1,39 +1,14 @@
 #include "traffic_lights.h"
 
+//initializes port B for t LED lights
+//input: none
+//output: none
 void traffic_init(void)
 {
     SYSCTL->RCGCGPIO |= 0x02;   // enable clock to GPIOF & D
     while ((SYSCTL->RCGCGPIO&0x02)==0){}
-    GPIOD->DIR |= 0x3F;
-    GPIOD->DEN |= 0x3F;
-}
-
-void SysTick_Init(void)
-{
-    SysTick->CTRL = 0; // 1) disable SysTick during setup
-    SysTick->LOAD = 0x00FFFFFF; // 2) maximum reload value
-    SysTick->VAL = 0; // 3) any write to CURRENT clears it
-    SysTick->CTRL = 0x00000005; // 4) enable SysTick with core clock
-}
-
-void PLL_init(void)
-{
-    SYSCTL->RCC2 |= 0x80000000;
-    SYSCTL->RCC2 |= 0x00000800;
-    SYSCTL->RCC=(SYSCTL->RCC~&x0x000007C0)+0x00000540;
-    SYSCTL->RCC2 &~0x00000070;
-    SYSCTL->RCC2&~0x00002000;
-    SYSCTL->RCC2|=0x40000000;
-    SYSCTL->RCC2=(SYSCTL->RCC2&~0x1FC0000)+(639<<22);
-    while ((SYSCTL->RIS&0x00000040)==0){};
-    SYSCTL->RCC2&=~0x00000800;
-}
-
-void SysTick_Wait(uint32_t delay)
-{
-    SysTick->LOAD = delay-1; // number of counts
-    SysTick->VAL = 0; // any value written to CURRENT clears
-    while((SysTick->CTRL&0x00010000)==0){} // wait for flag
+    GPIOB->DIR |= 0x3F;
+    GPIOB->DEN |= 0x3F;
 }
 
 //this mode lasts 10 seconds
@@ -41,7 +16,7 @@ void SysTick_Wait(uint32_t delay)
 //ouput: none
 void Red_Green(void)
 {
-    GPIOB->DATA = 0x11;
+    GPIOB->DATA = ~0x11;
 }
 
 //this mode lasts 2 seconds
@@ -49,7 +24,7 @@ void Red_Green(void)
 //ouput: none
 void Red_Yellow(void)
 {
-    GPIOB->DATA = 0x21;
+    GPIOB->DATA = ~0x21;
 }
 
 //this mode lasts 10 seconds
@@ -57,7 +32,7 @@ void Red_Yellow(void)
 //ouput: none
 void Green_Red(void)
 {
-    GPIOB->DATA = 0x0A;
+    GPIOB->DATA = ~0x0A;
 }
 
 //this mode lasts 2 seconds
@@ -65,7 +40,7 @@ void Green_Red(void)
 //ouput: none
 void Yellow_Red(void)
 {
-    GPIOB->DATA = 0x0C;
+    GPIOB->DATA = ~0x0C;
 }
 
 //this mode lasts 1 second
@@ -73,6 +48,6 @@ void Yellow_Red(void)
 //output: none
 void Red_Red(void)
 {
-    GPIOB->DATA = 0x09;
+    GPIOB->DATA = ~0x09;
 }
 
--- traffic_lights.h
+++ traffic_lights.h
@@ -1,11 +1,8 @@
 #include "TM4C123GH6PM.h"
 
 void traffic_init(void);
-void PLL_init(void);
 void Red_Green(void);
 void Green_Red(void);
 void Red_Red(void);
 void Red_Yellow(void);
 void Yellow_Red(void);
-void SysTick_Init(void);
-void SysTick_Wait (uint32_t delay);
