44262ddb8a13e1a0db461ac75e0bbc47692b2f76
MattConCarr3 <143846508+MattConCarr3@users.noreply.github.com>
2024-02-28 11:58:38 -0800

    Add files via upload

--- Lab6Ports/PortExpansion.c
+++ Lab6Ports/PortExpansion.c
@@ -4,18 +4,19 @@
 
 
 void SSI0_Init(void) {
-	SYSCTL->RCGCSSI |= 0x01; //Activate SSI0 - bit 0 for SSI0, 1 for SSI1 ...
+	SYSCTL->RCGCSSI |= 0x02; //Activate SSI0 - bit 0 for SSI0, 1 for SSI1 ...
 	SYSCTL->RCGCGPIO |= 0x12; /* enable clock to Port B and E */
 	while((SYSCTL->PRGPIO&0x11) == 0) {}; // ready?
 		GPIOB->AFSEL |= 0x0F; /* enable alternate function on PA2,3,5*/
 	GPIOB->PCTL = (GPIOA->PCTL&0xFF0F00FF)+0x00202200;
 	GPIOB->AMSEL &= ~0x0F; /* disable analog for these pins */
-	GPIOB->DEN |= 0x0F; /* enable digital I/O on PA2,3,5 */
-	SSI0->CR1 = 0x00; /* disable SSI and make it master */
-	SSI0->CPSR = 0x0A; /* 5MHz SSICLK; assume system clock 50MHz */
-	SSI0->CR0 &= ~(0x0000FFF0); // SSI, SPO = 0, SPH = 0; SCR=0
-	SSI0->CR0 = (SSI0->CR0 &~0x0F) + 0x07; /* 8 bit data */
-	SSI0->CR1 |= 0x02; /* enable SSI0 */
+	GPIOB->DEN |= 0x0F; /* enable digital I/O on PB 0-3*/
+	GPIOE->DEN |= 0x08; /* enable digital I/O on PE 3*/
+	SSI1->CR1 = 0x00; /* disable SSI and make it master */
+	SSI1->CPSR = 0x0A; /* 5MHz SSICLK; assume system clock 50MHz */
+	SSI1->CR0 &= ~(0x0000FFF0); // SSI, SPO = 0, SPH = 0; SCR=0
+	SSI1->CR0 = (SSI0->CR0 &~0x0F) + 0x07; /* 8 bit data */
+	SSI1->CR1 |= 0x02; /* enable SSI0 */
 }
 
 void SSI0_Write(unsigned char data) // output port expander function
