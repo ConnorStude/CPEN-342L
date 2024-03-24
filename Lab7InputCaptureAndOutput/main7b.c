364930634bc3c029a1ebc005aba42f6ad47404b6
marion-haviland <135187050+marion-haviland@users.noreply.github.com>
2024-03-24 14:46:25 -0700

    Create main7b.c
    //I think you need to calculate time this way, so you are dividing cycles by cycles per ms

---
+++ Lab7InputCaptureAndOutput/main7b.c
@@ -0,0 +1,33 @@
+
+#include "TM4C123GH6PM.h"
+#include "timers.h"
+#include "LCD.h"
+#include "switch_LED.h"
+#include <stdio.h>// include for int to char func
+
+extern volatile uint32_t time;
+extern volatile char updateFlag;
+
+void intToChar(int num, char *str) {
+  // Convert the integer to a string
+  sprintf(str, "%d", num);
+}
+
+int main(void) {
+	Ports_Init();
+	Timer0A_Init();
+	Timer1A_Init();
+	LCD_init();
+	char str[5];
+	while(1) {
+		if (updateFlag==1) {
+			int int_realtime = time / 50000;
+			intToChar(int_realtime,str);
+			LCD_command(0x80);
+			LCD_Str(str);
+			LCD_Str("ms");
+			updateFlag=0;
+		}
+	}
+	return 0;
+}
